ce64e4de4468f33a7e2622c882b5a7dbf96546d8
maximus <m.koretskyi@gmail.com>
2016-10-09 10:43:18 +0300

    parent logging is implemented

--- background/background.js
+++ background/background.js
@@ -1,15 +1,6 @@
-var config = {
-  apiKey: "AIzaSyC5N0FavZVS3L-FWaBXnLtuKgC0qJ9CDUE",
-  authDomain: "searchtrek.firebaseapp.com",
-  databaseURL: "https://searchtrek.firebaseio.com",
-  storageBucket: "searchtrek.appspot.com",
-  messagingSenderId: "612488784459"
-};
-firebase.initializeApp(config);
-var historyRawRef = firebase.database().ref('history-raw');
-
+var replaced = {};
 var activeTabIds = [undefined, undefined];
-const initActive = localStorage.getItem('active') === 'false';
+var initActive = localStorage.getItem('active') === 'false';
 var active = false;
 
 function init() {
@@ -17,8 +8,13 @@ function init() {
 
   chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
     if (active && tab.status == "complete" && info.status !== undefined && tab.url !== "chrome://newtab/") {
-      console.log(tabId, tab.url, info, Date.now(), tab.openerTabId, activeTabIds.join(","));
-      console.log('Tab:', tab);
+
+        var opener = tab.openerTabId;
+        if (!tab.openerTabId && replaced.hasOwnProperty(tab.id)) {
+            opener = replaced[tab.id];
+        }
+
+        console.log(tabId, tab.url, Date.now(), opener, activeTabIds.join(","));
 
       fetch('http://localhost:8080/api/log',
         {
@@ -34,7 +30,7 @@ function init() {
             title: tab.title,
             tabUrl: tab.url,
             updated: Date.now(),
-            openerTabId: tab.openerTabId || activeTabIds[0] || null,
+            openerTabId: opener || activeTabIds[0] || null,
             activeTabIds: activeTabIds.join(",")
           })
         })
@@ -44,34 +40,29 @@ function init() {
         .catch(function (ex) {
           console.log('post history failed', ex)
         });
-
-      /*historyRawRef.push({
-       tabId: tabId,
-       favIconUrl: tab.favIconUrl || "",
-       title: tab.title,
-       tabUrl: tab.url,
-       updated: Date.now(),
-       openerTabId: tab.openerTabId || activeTabIds[0] || null,
-       activeTabIds: activeTabIds.join(",")
-       });*/
     }
   });
 
-  chrome.tabs.onActivated.addListener(function (info) {
-    activeTabIds.pop();
-    activeTabIds.unshift(info.tabId);
-    if (active) {
-      console.log('active tab changed to ', activeTabIds.join(","));
-    }
-  });
 
-  /*
-   chrome.tabs.onReplaced.addListener(function (addedTabId, removedTabId) {
-   var index = activeTabIds.indexOf(removedTabId);
-   activeTabIds.splice(index, 1, addedTabId);
-   console.log('replaced tabs: ', activeTabIds.join(","));
-   });
-   */
+    chrome.tabs.onActivated.addListener(function (info) {
+        chrome.tabs.get(info.tabId, function (tab) {
+            if (tab.status === 'complete') {
+                activeTabIds.pop();
+                activeTabIds.unshift(info.tabId);
+                console.log('active tab changed to ', activeTabIds.join(","));
+            } else if (tab.url === "chrome://newtab/") {
+                replaced[tab.id] = activeTabIds[0];
+            }
+        });
+    });
+
+    chrome.tabs.onReplaced.addListener(function (addedTabId, removedTabId) {
+        if (replaced.hasOwnProperty(removedTabId)) {
+            replaced[addedTabId] = replaced[removedTabId];
+            delete replaced[removedTabId];
+            console.log('replaced tab ', removedTabId, ' to ', addedTabId);
+        }
+    });
 
   chrome.browserAction.onClicked.addListener(function (tab) {
     setActive(!active);
